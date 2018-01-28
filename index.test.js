2db8c3e1499bf6b81982bbd05a5643b4d59d8888
Suchipi Izumi <me@suchipi.com>
2018-01-27 21:43:26 -0700

    Add async function tests

--- index.test.js
+++ index.test.js
@@ -30,6 +30,15 @@ describe("run-on-server", () => {
         args: [1, 2, 3],
         expected: [2, 4, 6]
       },
+      "async arrow function without block body": {
+        code: async () => await Promise.resolve(5),
+        expected: 5
+      },
+      "async arrow function without block body with args": {
+        code: async (a, b, c) => await Promise.resolve([a, b, c]),
+        args: [1, 2, 3],
+        expected: [1, 2, 3]
+      },
       "arrow function with block body": {
         code: () => {
           return 5;
@@ -43,6 +52,21 @@ describe("run-on-server", () => {
         args: [1, 2, 3],
         expected: [2, 4, 6]
       },
+      "async arrow function with block body": {
+        code: async () => {
+          const number = await Promise.resolve(5);
+          return number;
+        },
+        expected: 5
+      },
+      "async arrow function with block body with args": {
+        code: async (a, b, c) => {
+          const array = await Promise.resolve([a, b, c]);
+          return array;
+        },
+        args: [1, 2, 3],
+        expected: [1, 2, 3]
+      },
       "function expression": {
         code: function() {
           return 5;
@@ -55,6 +79,19 @@ describe("run-on-server", () => {
         },
         args: [1, 2, 3],
         expected: [2, 4, 6]
+      },
+      "async function expression": {
+        code: async function() {
+          return await Promise.resolve(5);
+        },
+        expected: 5
+      },
+      "async function expression with args": {
+        code: async function(a, b, c) {
+          return await Promise.resolve(a + b + c);
+        },
+        args: [1, 2, 3],
+        expected: 6
       },
       "function declaration": {
         code: (() => {
@@ -70,6 +107,28 @@ describe("run-on-server", () => {
         code: (() => {
           function foo(a, b, c) {
             return [a, b, c].map(x => x * 2);
+          }
+
+          return foo;
+        })(),
+        args: [1, 2, 3],
+        expected: [2, 4, 6]
+      },
+      "async function declaration": {
+        code: (() => {
+          async function foo() {
+            return await Promise.resolve(5);
+          }
+
+          return foo;
+        })(),
+        expected: 5
+      },
+      "async function declaration with args": {
+        code: (() => {
+          async function foo(a, b, c) {
+            const doubled = await Promise.resolve([a, b, c].map(x => x * 2));
+            return doubled;
           }
 
           return foo;
