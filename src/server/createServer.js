3857ce5136abcc5ba8dc2fa7cc7b502c11727cff
Suchipi Izumi <me@suchipi.com>
2018-01-29 23:41:36 -0700

    Add some more flow types, fix bug where 0 couldn't be sent back

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const executeCode = require("./executeCode");
 import type { $Application, $Request, $Response } from "express";
-import type { ServerConfig, APIRequest } from "../types";
+import type { ServerConfig, APIRequest, APIResponse } from "../types";
 
 module.exports = function createServer(
   serverConfig: ?ServerConfig
@@ -14,20 +14,24 @@ module.exports = function createServer(
   app.post("/", (req: $Request & { body: APIRequest }, res: $Response) => {
     executeCode(req.body, serverConfig)
       .then((result) => {
-        res.status(200).send({
-          success: true,
-          result: result || null,
-        });
+        res.status(200).send(
+          ({
+            success: true,
+            result: result,
+          }: APIResponse)
+        );
       })
-      .catch((err) => {
-        res.status(500).send({
-          success: false,
-          err: {
-            name: err.name,
-            message: err.message,
-            stack: err.stack,
-          },
-        });
+      .catch((err: Error) => {
+        res.status(500).send(
+          ({
+            success: false,
+            err: {
+              name: err.name,
+              message: err.message,
+              stack: err.stack,
+            },
+          }: APIResponse)
+        );
       });
   });
 
