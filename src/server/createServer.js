c3be2e37da496450b2786477aa0e88e71c341a93
Suchipi Izumi <me@suchipi.com>
2018-01-30 21:24:56 -0700

    Flow fix

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -2,7 +2,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const executeCode = require("./executeCode");
-import type { $Application, $Request, $Response } from "express";
+import type {
+  $Application,
+  // eslint-disable-next-line no-unused-vars
+  $Request,
+  $Response,
+} from "express";
 import type { ServerConfig, APIRequest, APIResponse } from "../types";
 
 module.exports = function createServer(
@@ -11,29 +16,32 @@ module.exports = function createServer(
   const app = express();
   app.use(bodyParser.json());
 
-  app.post("/", (req: $Request & { body: APIRequest }, res: $Response) => {
-    executeCode(req.body, serverConfig)
-      .then((result) => {
-        res.status(200).send(
-          ({
-            success: true,
-            result: typeof result === "undefined" ? null : result,
-          }: APIResponse)
-        );
-      })
-      .catch((err: Error) => {
-        res.status(500).send(
-          ({
-            success: false,
-            err: {
-              name: err.name,
-              message: err.message,
-              stack: err.stack,
-            },
-          }: APIResponse)
-        );
-      });
-  });
+  app.post(
+    "/",
+    (req: { /* :: ...$Request, */ body: APIRequest }, res: $Response) => {
+      executeCode(req.body, serverConfig)
+        .then((result) => {
+          res.status(200).send(
+            ({
+              success: true,
+              result: typeof result === "undefined" ? null : result,
+            }: APIResponse)
+          );
+        })
+        .catch((err: Error) => {
+          res.status(500).send(
+            ({
+              success: false,
+              err: {
+                name: err.name,
+                message: err.message,
+                stack: err.stack,
+              },
+            }: APIResponse)
+          );
+        });
+    }
+  );
 
   return app;
 };
